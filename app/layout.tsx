8cf2f32a0747ba13daf5dd76a88586eb4115c3df
mike <jfkz@users.noreply.github.com>
2025-05-04 12:19:08 +0000

    feat: update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Buy Me A Vodka',
+  description: 'Buy me a vodka, comrade! Mint an NFT for a comrade and support the cause.',
   generator: 'v0.dev',
 }
 
